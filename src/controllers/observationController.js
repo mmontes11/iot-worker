c40d9b7c1bffee844680a1348c9052ae64f364b2
mmontes11 <martin11lrx@gmail.com>
2018-04-08 16:33:33 +0200

    Inserting measurement changes

--- src/controllers/observationController.js
+++ src/controllers/observationController.js
@@ -2,8 +2,11 @@ import matchTopic from "mqtt-match";
 import _ from 'underscore';
 import { TopicController} from "./topicController"
 import subscriptionModel from "../models/subscriptionModel";
+import measurementModel from "../models/measurementModel";
+import measurementChangeModel from "../models/measurementChangeModel";
 import biotClient from '../lib/biotClient';
 import log from '../utils/log';
+import measurement from "../../../iot-server/test/constants/measurement";
 
 class ObservationController extends TopicController {
     async getNotifications(topic, observation) {
@@ -37,8 +40,33 @@ class EventController extends ObservationController {
 class MeasurementController extends ObservationController {
     async handleTopic(topic, measurement) {
         try {
+            await Promise.all([
+                this._sendMeasurementNotifications(topic, measurement),
+                this._processMeasurementChange(measurement)
+            ]);
+        } catch (err) {
+            throw err;
+        }
+    }
+    async _sendMeasurementNotifications(topic, measurement) {
+        try {
             const notifications = await super.getNotifications(topic, measurement);
             await biotClient.sendMeasurementNotifications(notifications);
+        } catch (err) {
+            throw err;
+        }
+    }
+    async _processMeasurementChange(measurement) {
+        try {
+            const growthRate = await measurementModel.getMeasurementGrowthRate(measurement);
+            if (!_.isUndefined(growthRate)) {
+                const measurementChange = {
+                    observation: measurement,
+                    growthRate,
+                    phenomenonTime: new Date()
+                };
+                await measurementChangeModel.insertOne(measurementChange);
+            }
         } catch (err) {
             throw err;
         }
---
+++ src/models/measurementModel.js
@@ -0,0 +1,42 @@
+import _ from "underscore";
+import { MongoModel } from "./mongoModel";
+import { ObjectID } from 'mongodb';
+import mongo from '../lib/mongo';
+
+class Measurement extends MongoModel {
+    async getMeasurementGrowthRate(currentMeasurement) {
+        const currentObjectId = new ObjectID(currentMeasurement._id);
+        const pastDate = new Date();
+        pastDate.setHours(pastDate.getHours() - 1);
+        const pipeline = [
+            {
+                $match: {
+                    $and: [
+                        { _id: { $ne: currentObjectId }},
+                        { phenomenonTime: { $gte: pastDate }},
+                        { thing: { $eq: currentMeasurement.thing }},
+                        { type: { $eq: currentMeasurement.type }},
+                        { 'unit.name': { $eq: currentMeasurement.unit.name }}
+                    ]
+                }
+            },
+            {
+                $group: {
+                    _id: null,
+                    avgValue: { $avg: "$value" }
+                }
+            }
+        ];
+        const aggregationResult = await this.collection().aggregate(pipeline).toArray();
+        const pastAvg = _.first(aggregationResult).avgValue;
+        if (!_.isUndefined(pastAvg)) {
+            return (currentMeasurement.value - pastAvg) / pastAvg;
+        } else {
+            return undefined;
+        }
+    }
+}
+
+const measurement = new Measurement(mongo, 'measurements');
+
+export default measurement;
